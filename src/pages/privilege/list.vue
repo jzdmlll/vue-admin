b99073c8732bc76a2a0b5667afc316df5c657c29
jzdmlll <1037617790@qq.com>
2020-09-04 17:10:32 +0800

    lzc-feature-20200904

--- src/pages/privilege/list.vue
+++ src/pages/privilege/list.vue
@@ -3,28 +3,29 @@
   <div class="privilege_list">
     <div class="btns" style="padding:1em;margin-bottom:1em;background:#fff">
       <el-tooltip class="item" effect="dark" content="添加权限" placement="bottom-start">
-        <el-button type="primary" icon="el-icon-plus" size="mini" @click="toAdd" ></el-button>
+        <el-button type="primary" icon="el-icon-plus" size="mini" @click="toAdd" />
       </el-tooltip>
     </div>
     <div style="padding:1em;margin-bottom:1em;background:#fff">
       <el-table
-        :data="privileges"
         v-loading="loading"
+        :data="privileges"
         size="small"
         :lazy="true"
         row-key="id"
         :load="lazyLoadPrivilege"
-        :tree-props="{children: 'children', hasChildren: 'hasChildren'}">
-        <el-table-column prop="name" label="名称"></el-table-column>
-        <el-table-column prop="route" label="路径"></el-table-column>
-        <el-table-column prop="type" label="类型"></el-table-column>
+        :tree-props="{children: 'children', hasChildren: 'hasChildren'}"
+      >
+        <el-table-column prop="name" label="名称" />
+        <el-table-column prop="route" label="路径" />
+        <el-table-column prop="type" label="类型" />
         <el-table-column label="操作" align="center" width="180">
           <template slot-scope="scope">
             <el-tooltip class="item" effect="dark" content="删除权限" placement="bottom-start">
-              <el-button type="danger" icon="el-icon-delete" size="mini" @click="deleteHandler(scope.row.id)"></el-button>
+              <el-button type="danger" icon="el-icon-delete" size="mini" @click="deleteHandler(scope.row.id)" />
             </el-tooltip>
             <el-tooltip class="item" effect="dark" content="修改权限" placement="bottom-start">
-              <el-button type="success" icon="el-icon-edit" size="mini" @click="toEdit(scope.row)"></el-button>
+              <el-button type="success" icon="el-icon-edit" size="mini" @click="toEdit(scope.row)" />
             </el-tooltip>
           </template>
         </el-table-column>
@@ -34,30 +35,30 @@
     <el-dialog :title="title" :visible.sync="visible">
       <el-form :model="form">
         <el-form-item label="名称" label-width="80px">
-          <el-input v-model="form.name" autocomplete="off"></el-input>
+          <el-input v-model="form.name" autocomplete="off" />
         </el-form-item>
         <el-form-item label="路径" label-width="80px">
-          <el-input v-model="form.route" autocomplete="off"></el-input>
+          <el-input v-model="form.route" autocomplete="off" />
         </el-form-item>
         <el-form-item label="类型" label-width="80px">
           <el-select v-model="form.type" clearable placeholder="请选择">
-            <el-option label="父级" value="parent"></el-option>
-            <el-option label="菜单" value="menu"></el-option>
-            <el-option label="方法" value="method"></el-option>
+            <el-option label="父级" value="parent" />
+            <el-option label="菜单" value="menu" />
+            <el-option label="方法" value="method" />
           </el-select>
         </el-form-item>
         <el-form-item label="父权限" label-width="80px">
           <el-select v-model="form.parentId" clearable placeholder="请选择">
-            <el-option v-for="p in privileges" :key="p.id" :label="p.name" :value="p.id"></el-option>
+            <el-option v-for="p in privileges" :key="p.id" :label="p.name" :value="p.id" />
           </el-select>
         </el-form-item>
         <el-form-item label="描述" label-width="80px">
-          <el-input type="textarea" v-model="form.remark" autocomplete="off"></el-input>
+          <el-input v-model="form.remark" type="textarea" autocomplete="off" />
         </el-form-item>
       </el-form>
       <div slot="footer" class="dialog-footer">
-        <el-button @click="visible = false" size="small">取 消</el-button>
-        <el-button type="primary" @click="submitHandler" size="small">确 定</el-button>
+        <el-button size="small" @click="visible = false">取 消</el-button>
+        <el-button type="primary" size="small" @click="submitHandler">确 定</el-button>
       </div>
     </el-dialog>
     <!-- /模态框 -->
@@ -115,7 +116,7 @@ export default {
       request.get('/privilege/findByParentId')
         .then(response => {
           response.data.forEach(item => {
-            item.hasChildren = !Boolean(item.parentId)
+            item.hasChildren = !item.parentId
           })
           this.privileges = response.data
           this.loading = false
